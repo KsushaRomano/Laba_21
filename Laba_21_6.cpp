bcfbc46bf31931706cd7a1bbe193b548e6f10d19
KsushaRomano <55737387+KsushaRomano@users.noreply.github.com>
2019-10-22 00:01:04 +0300

    Add files via upload

---
+++ Laba_21_6.cpp
@@ -0,0 +1,32 @@
+﻿using namespace std;
+
+#include <iostream>
+#include <string>
+#include <windows.h>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int i, m, k;
+	string S;
+	k = 0;
+	cout << "Введите путь к файлу: ";
+	getline(cin, S);
+	m = S.size();
+	for (i = 0; i < m; i++) {
+		if (S[i] == 92) {
+			k++;
+		};
+	};
+	if (k == 1) {
+		cout << char(92);
+	}
+	else {
+		cout << "Имя последнего каталога: ";
+		for (i = S.find_last_of(92, S.find_last_of(92) - 1) + 1; i < S.find_last_of(92); i++) {
+			cout << S[i];
+		};
+	};
+}

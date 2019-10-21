f5a0d8df86674d9bf64e922ce498d0168e0e3db2
KsushaRomano <55737387+KsushaRomano@users.noreply.github.com>
2019-10-21 23:15:18 +0300

    Update Laba_21_2.cpp

--- Laba_21_2.cpp
+++ Laba_21_2.cpp
@@ -2,10 +2,13 @@
 
 #include <iostream>
 #include <string>
+#include <windows.h>
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 	int k, m, i, min;
 	string S;
 	k = 0;
